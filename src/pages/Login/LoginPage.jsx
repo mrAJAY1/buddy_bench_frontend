b7793d82240bb0a34a7bea0f8bb9fbcca1e49d3e
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-05-29 08:31:38 +0530

    login page commit 2

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,4 +1,6 @@
 import Button from "@mui/material/Button";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 import Style from "./LoginStyle.module.scss";
 
 function LoginPage() {
@@ -8,23 +10,35 @@ function LoginPage() {
         <form action="">
           <label htmlFor="username">
             Username
-            <input id="username" name="username" type="text" />
-            <i />
+            <div>
+              <input id="username" name="username" type="text" />
+              <i />
+            </div>
           </label>
           <label htmlFor="password">
             Password
-            <input id="password" name="password" type="text" />
-            <i />
+            <div>
+              <input id="password" name="password" type="text" />
+              <i />
+            </div>
           </label>
-          <div>
-            <Button
-              sx={{ width: { sm: 200, lg: 500, md: 300 } }}
-              variant="contained"
-              color="primary"
-            >
-              Button
-            </Button>
-          </div>
+          <Button sx={{ width: "100%" }} variant="contained" color="primary">
+            Button
+          </Button>
+          <FormControlLabel
+            label="Remember me"
+            sx={{
+              flexDirection: "row !important",
+            }}
+            control={
+              <Checkbox
+                value=""
+                // checked={}
+                // onChange={}
+                color="primary"
+              />
+            }
+          />
         </form>
       </div>
     </div>
